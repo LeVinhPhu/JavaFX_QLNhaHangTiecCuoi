69061b4898a77e448bcd4badd65e0056b521f8bb
Van Quoc <nguyenvanquoc2388@gmail.com>
2021-12-06 08:48:52 +0700

    update

---
+++ QLNhaHangTiecCuoi/src/main/java/com/mycompany/conf/demothu.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.conf;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class demothu {
+    private int aiBiet;
+}
---
+++ QLNhaHangTiecCuoi/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.mycompany.qlnhahangtieccuoi {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires java.base;
+
+    opens com.mycompany.qlnhahangtieccuoi to javafx.fxml;
+    exports com.mycompany.qlnhahangtieccuoi;
+}
