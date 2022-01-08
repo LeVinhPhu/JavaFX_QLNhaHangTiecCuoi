83af20601807b176b2c53b3566c7759ce80bb025
Van Quoc <nguyenvanquoc2388@gmail.com>
2022-01-09 03:58:52 +0700

    giao diện đã hoàn chỉnh: 99%, chưa thực hiện code

--- QLNhaHangTiecCuoi/src/main/java/com/mycompany/qlnhahangtieccuoi/DatTiecController.java
+++ QLNhaHangTiecCuoi/src/main/java/com/mycompany/qlnhahangtieccuoi/DatTiecController.java
@@ -14,16 +14,17 @@ import com.mycompany.services.PaymentMethodService;
 import com.mycompany.services.SanhCuoiService;
 import java.net.URL;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.property.ObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -51,10 +52,9 @@ public class DatTiecController implements Initializable {
     @FXML private Text slBanTD; 
     @FXML private Text tienThanhToan; 
     @FXML private DatePicker dayParty;
+    private double totalFoodAndService;
     private double totalCost;
-    private double totalPrepayment;
-    private int soBan = 0;
-    private double price;
+    
     
     
 
@@ -64,6 +64,10 @@ public class DatTiecController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        //this.dayParty.setMinDate(System.currentTimeMillis() - 1000);
+        //this.dayParty.setDayCellFactory(cf -> new Min);
+        DayLimit();
+        this.txtSoBan.setText(null);
         LoadTableViewService();
         LoadTableViewFood();
         SanhCuoiService sc = new SanhCuoiService();
@@ -90,7 +94,24 @@ public class DatTiecController implements Initializable {
             } catch (SQLException ex) {
                 Logger.getLogger(QLMonAnController.class.getName()).log(Level.SEVERE, null, ex);
             }
-        });     
+        });
+        this.txtSoBan.textProperty().addListener((evt)->{
+            if (this.txtSoBan.getText().length() > 0){                
+                int tableNum = Integer.parseInt(this.txtSoBan.getText());
+                if (tableNum > Integer.parseInt(this.slBanTD.getText())){
+                    this.txtSoBan.setText(this.slBanTD.getText());
+                    tableNum = Integer.parseInt(this.slBanTD.getText());;
+                }                    
+                totalCost = totalFoodAndService + tableNum * Double.parseDouble(this.donGiaBan.getText());
+                this.tongTien.setText(String.valueOf(String.format("%.0f", totalCost)));
+            } 
+            else{
+                this.txtSoBan.setText("0");
+                this.tongTien.setText(String.valueOf(totalFoodAndService));
+            }
+
+        
+        });
         
     }
     
@@ -110,8 +131,6 @@ public class DatTiecController implements Initializable {
         TableColumn colSelect = new TableColumn("Chọn");
         colSelect.setCellValueFactory(new PropertyValueFactory("select"));
         colSelect.setPrefWidth(100);
-        colSelect.setId("disa");
-        //colSelect.setStyle(string);
         
         this.tbFood.getColumns().addAll(colName, colPrice, colNote, colSelect);
     }
@@ -145,6 +164,7 @@ public class DatTiecController implements Initializable {
     @FXML
     private void handleClickTableViewService(MouseEvent click){
         Services service = this.tbService.getSelectionModel().getSelectedItem();
+        double temp;
         if (service != null){
             if (service.getSelect().isSelected())                
                 service.getSelect().setSelected(false);  //Chuyển trạng thái của checkbox                            
@@ -152,19 +172,25 @@ public class DatTiecController implements Initializable {
                 service.getSelect().setSelected(true); //Chuyển trạng thái của checkbox
         }
         if (service.getSelect().isSelected()){
-            this.totalCost += service.getUnitPrice();
-            this.tongTien.setText(String.valueOf(totalCost));
+            this.totalFoodAndService += service.getUnitPrice();
+            temp = totalFoodAndService + totalCost;
+            this.tongTien.setText(String.valueOf(String.format("%.0f", temp)));
+            getPaymentID();            
         }
         else{
-            this.totalCost -= service.getUnitPrice();
-            if (totalCost > 0)
-                this.tongTien.setText(String.valueOf(totalCost));
-            else this.tongTien.setText(String.valueOf(0));
+            this.totalFoodAndService -= service.getUnitPrice();
+            if (totalFoodAndService > 0)
+                temp = totalCost - totalFoodAndService;
+            else
+                temp = Integer.parseInt(this.txtSoBan.getText()) * Double.parseDouble(this.donGiaBan.getText());
+            this.tongTien.setText(String.valueOf(String.format("%.0f", temp)));
+            getPaymentID();
         }
     }
     @FXML
-    private void handleClickTableViewFood(MouseEvent click){
+    private void handleClickTableViewFood(MouseEvent click){ //chưa xửa xong
         Food food = this.tbFood.getSelectionModel().getSelectedItem();
+        double temp = 0;
         if (food != null){
             if (food.getSelect().isSelected())                
                 food.getSelect().setSelected(false);  //Chuyển trạng thái của checkbox                            
@@ -172,36 +198,22 @@ public class DatTiecController implements Initializable {
                 food.getSelect().setSelected(true); //Chuyển trạng thái của checkbox
         }
         if (food.getSelect().isSelected()){
-            this.totalCost += food.getUnitPrice();
-            this.tongTien.setText(String.valueOf(totalCost));
+            this.totalFoodAndService += food.getUnitPrice();
+            temp = totalFoodAndService + totalCost;
+            this.tongTien.setText(String.valueOf(String.format("%.0f", temp)));
+            getPaymentID();
         }
         else{
-            this.totalCost -= food.getUnitPrice();
-            if (totalCost > 0)
-                this.tongTien.setText(String.valueOf(totalCost));
-            else this.tongTien.setText(String.valueOf(0));
-        }
-    }
-    
-    @FXML
-    private void BtdLuu(ActionEvent event) throws SQLException{
-        List<Services> listService = new ArrayList<>();
-        DichVuServices dv = new DichVuServices();
-        listService = dv.getServices(null);
-        for (Services s : listService){
-            if (s.getSelect().isSelected()){
-                this.totalCost += s.getUnitPrice();                
+            this.totalFoodAndService -= food.getUnitPrice();
+            if (totalFoodAndService > 0){
+                temp = totalCost - totalFoodAndService;
+                totalCost = temp;
             }
+            else
+                totalCost = Integer.parseInt(this.txtSoBan.getText()) * Double.parseDouble(this.donGiaBan.getText());
+            this.tongTien.setText(String.valueOf(String.format("%.0f", temp)));
+            getPaymentID();
         }
-        List<Food> listFood = new ArrayList<>();
-        FoodService food = new FoodService();
-        listFood = food.getFood(null);
-        for (Food f : listFood){
-            if (f.getSelect().isSelected()){
-                this.totalCost += f.getUnitPrice();                
-            }
-        }
-        this.tongTien.setText(String.valueOf(totalCost));
     }
     
     @FXML
@@ -218,12 +230,33 @@ public class DatTiecController implements Initializable {
         if (sc != null){
             this.donGiaBan.setText(String.valueOf(sc.getUnitPrice()));
             this.slBanTD.setText(String.valueOf(sc.getSoBanToiDa()));
+            this.txtSoBan.setText(this.slBanTD.getText());
         }
     }
     @FXML
     private void EventComboBox(ActionEvent event){
         getSanhCuoiID();
-        this.price = Double.parseDouble(this.donGiaBan.getText());
+    }
+    
+    private void getPaymentID(){
+        PaymentMethods pay = this.cbPhuongThucTT.getSelectionModel().getSelectedItem();
+        if(this.cbSanhCuoi != null){
+            if (pay != null){
+                int payID = pay.getPaymentID();
+                if (payID == 1){
+                    this.tienThanhToan.setText(this.tongTien.getText());
+                }
+                else{
+                   double money = Double.parseDouble(this.tongTien.getText());
+                   money *= 0.1;
+                   this.tienThanhToan.setText(String.valueOf(String.format("%.0f", money)));
+                }
+            }
+        }
+    }
+    @FXML
+    private void EventTienThanhToan(ActionEvent event){
+        getPaymentID();
     }
     
     @FXML
@@ -234,13 +267,39 @@ public class DatTiecController implements Initializable {
         }
     });
     }
-    @FXML
-    private void TextChanged(KeyEvent keyEvent){
-        this.txtSoBan.textProperty().addListener((evt)->{
-            this.soBan = Integer.getInteger(this.txtSoBan.getText());
-            this.totalCost += soBan * price;
-            this.tongTien.setText(String.valueOf(totalCost));
-        });
+    
+    private void init(){
+        this.txtSoBan.setText("0");
+        this.totalFoodAndService = 0;
+        this.totalCost = 0;
+        this.txtKeywordFood.setText(null);
+        this.txtKeywordService.setText(null);
     }
 
+    private class MinDateCell extends DateCell {
+
+        private ObjectProperty<LocalDate> date;
+
+        private MinDateCell(ObjectProperty<LocalDate> date) {
+            this.date = date;
+        }
+
+        @Override
+        public void updateItem(LocalDate item, boolean empty) {
+            super.updateItem(item, empty);
+            if (item.isBefore(date.get())) {
+                this.setDisable(true);
+                setStyle("-fx-background-color: #7e7e7e;"); // I used a different coloring to see which are disabled.
+            }
+        }
+
+    }
+    
+    private void DayLimit(){
+        this.dayParty.setDayCellFactory(cf -> {
+            DatePicker dayNow = new DatePicker();
+            dayNow.setValue(LocalDate.now());
+            return new MinDateCell(dayNow.valueProperty());
+        });
+    }
 }
